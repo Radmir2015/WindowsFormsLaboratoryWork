21c059e00ae1cc13d7e0d2c28b05a42a2aa216f6
Radmir2015 <ya.radmir2015@yandex.ru>
2017-09-25 12:36:42 +0300

    laba4 create

---
+++ laba4/laba4/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba4
+{
+    public partial class Form1 : Form
+    {
+        int[] staticArray = new int[10];
+        int[] dinamicArray = { };
+
+        int number = 0;
+        int cxS = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out number))
+            {
+                int[] tempArray = new int[dinamicArray.Length + 1];
+                dinamicArray.CopyTo(tempArray, 0); // Копируем старый массив в новый
+                dinamicArray = tempArray;
+                dinamicArray[dinamicArray.Length - 1] = number;
+            }
+            label2.Text = "Массив: " + string.Join(", ", dinamicArray);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out number))
+            {
+                if (cxS >= 10)
+                {
+                    staticArray.Select(x => 0);
+                    cxS = 0;
+                }
+                else
+                {
+                    staticArray[cxS] = number;
+                    cxS++;
+                }
+            }
+            label1.Text = "Массив: " + string.Join(", ", staticArray);
+        }
+    }
+}
